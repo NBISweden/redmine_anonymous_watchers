44977982fb35b6f5ed079eecbeb66abd3f66c635
airenzp <airenzp@gmail.com>
2021-10-15 12:16:23 +0200

    creating an user or loading the user created, when adding or editing an issue, to make the mailer work

--- lib/redmine_anonymous_watchers/mailer_extension.rb
+++ lib/redmine_anonymous_watchers/mailer_extension.rb
@@ -2,7 +2,36 @@ module RedmineAnonymousWatchers
   module MailerExtension
     #unloadable
 
+    def create_user(mail)
+      user = User.new
+      names = mail.gsub(/@.*$/, '')
+      puts names
+      puts names.class
+      names_list = names.include?(".") ? names.split('.') : [names, '-']
+      user.login = mail
+      user.firstname = names_list[0]
+      user.lastname = names_list[1]
+      user.language = Setting.default_language
+      user.generate_password = true
+      user.mail_notification = 'only_my_events'
+      user.mail = mail
+      user.save!
+      user.lock!
+      user
+    end
     
+    def deliver_issue_add(issue)
+      users = issue.watcher_mails
+      users.each do |user|
+        unless user.is_a?(User)
+          mail = user
+          user = User.find_by_mail(mail)
+          user = create_user(mail) unless user
+        end
+        issue_add(user, issue).deliver_later
+      end      
+    end
+            
 
     def deliver_issue_edit(journal)
 
@@ -15,26 +44,14 @@ module RedmineAnonymousWatchers
           puts user
           unless user.is_a?(User)
             mail = user
-            user = User.new
-            names = mail.gsub(/@.*$/, '').split('.')
-            user.login = mail
-            user.firstname = names
-            user.lastname = '-'
-            user.language = Setting.default_language
-            user.generate_password = true
-            user.mail_notification = 'only_my_events'
-            user.mail = mail
-            user.save!
+            user = User.find_by_mail(mail)
+            user = create_user(mail) unless user
           end
-
           issue_edit(user, journal).deliver_later
         end
       end
-
-      super
     end
 
-    
   end
 end
 
